a8ea324598e8e4f86b0b02ecd4123f7fc3ba4bf1
Maxime Kohlhaas <maxime@atm-consulting.fr>
2016-08-24 17:07:33 +0200

    Tests synchro produits

--- class/syncsage.class.php
+++ class/syncsage.class.php
@@ -15,16 +15,71 @@ class TSyncSage {
 	
 	/***************************************************************************************
 	 * Fonctions concernant les produits
+	 * - Synchro Sage => Dolibarr avec création / maj des produits
 	 ***************************************************************************************/
 	
 	/*
 	 * Récupération de la liste des produit dans la base Sage 
 	 */
 	function get_product_from_sage() {
-		$sql = '';
+		$sql = 'SELECT a.*,ae.*,ag1.*,ag2.*';
+		$sql.= ' FROM [POLYPAP_DOLIBARR].[dbo].[F_ARTICLE] a';
+		$sql.= ' LEFT JOIN [POLYPAP_DOLIBARR].[dbo].[F_ARTENUMREF] ae ON (ae.AR_Ref = a.AR_Ref)';
+		$sql.= ' LEFT JOIN [POLYPAP_DOLIBARR].[dbo].[F_ARTGAMME] ag1 ON (ag1.AG_No = ae.AG_No1)';
+		$sql.= ' LEFT JOIN [POLYPAP_DOLIBARR].[dbo].[F_ARTGAMME] ag2 ON (ag2.AG_No = ae.AG_No2)';
+		$sql.= ' LIMIT 10';
 		
-		$this->sagedb->Execute($sql);
+		return $this->sagedb->ExecuteAsArray($sql,array(),PDO::FETCH_ASSOC);
+	}
+	
+	/*
+	 * Création / MAJ d'un produit dans Dolibarr
+	 */
+	function create_product_in_dolibarr($data) {
+		global $db,$user;
 		
-		return $this->sagedb->Get_All();
+		$p = new Product($db);
+		$p->fetch(0,$data['ref']);
+		
+		foreach($data as $k => $v) {
+			$p->{$k} = $v;
+		}
+		
+		if($p->id > 0) {
+			$res = $p->update($p->id, $user);
+		} else {
+			$res = $p->create($user);
+		}
+		
+		if($res < 0) {
+			echo '<br>ERR '.$p->error;
+		}
+	}
+	
+	/*
+	 * Construction d'une référence unique pour Dolibarr dans le cas d'une utilisation de gamme dans Sage
+	 */
+	function build_product_ref($dataline) {
+		$ref = $dataline['AR_Ref'];
+		if(!empty($dataline['AG_No1'])) {
+			$ref.= '_'.$dataline['AG_No1'];
+		}
+		if(!empty($dataline['AG_No2'])) {
+			$ref.= '_'.$dataline['AG_No2'];
+		}
+		
+		return $ref;
+	}
+	
+	function build_product_label($dataline) {
+		$label = $dataline['AR_Ref'];
+		if(!empty($dataline['EG_Enumere'])) {
+			$label.= ' - '.$dataline['EG_Enumere'];
+		}
+		if(!empty($dataline['EG_Enumere'])) {
+			$label.= ' - '.$dataline['EG_Enumere'];
+		}
+		
+		return $label;
 	}
 }
--- script/sync_product.php
+++ script/sync_product.php
@@ -7,4 +7,13 @@ require('../config.php');
 dol_include_once('/syncsage/class/syncsage.class.php');
 
 $sync = new TSyncSage();
-pre($sync,true);
+$TProduct = $sync->get_product_from_sage();
+
+foreach ($TProduct as $dataline) {
+	pre($dataline,true);
+	/*$data = array(
+		'ref'				=> $sync->build_product_ref($dataline)
+		,'label'			=> $sync->build_product_label($dataline)
+		,'barcode'			=> $dataline['AE_CodeBarre']
+	);*/
+}
