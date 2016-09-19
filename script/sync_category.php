d9e9b388eef0a8d9fbd6e957bfce3cca4c4628a8
Maxime Kohlhaas <maxime@atm-consulting.fr>
2016-09-20 00:02:46 +0200

    inclusion classe catégorie

--- script/sync_category.php
+++ script/sync_category.php
@@ -5,7 +5,7 @@ define('INC_FROM_CRON_SCRIPT', true);
 
 require('../config.php');
 dol_include_once('/syncsage/class/syncsage.class.php');
-dol_include_once('/categorie/class/categorie.class.php');
+dol_include_once('/categories/class/categorie.class.php');
 
 $sync = new TSyncSage();
 
